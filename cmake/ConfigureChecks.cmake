4fea603222a4bbd100cbd7107bfab346ab3fb5fa
Dmitry Baryshnikov <bishop.dev@gmail.com>
2019-06-18 13:59:00 +0300

    Fix bzip2 build

--- cmake/ConfigureChecks.cmake
+++ cmake/ConfigureChecks.cmake
@@ -33,8 +33,8 @@ include(FindAnyProject)
 # Find any dependencies
 if(USE_SYSTEM_BZip2)
     find_anyproject(BZip2 REQUIRED NAMES BZ2 bz2 libzip2)
-    set(BZIP2_INCLUDE_DIR ${BZ2_INCLUDE_DIRS})
-    set(BZIP2_LIBRARIES ${BZ2_LIBRARIES})
+    set(BZIP2_INCLUDE_DIR ${BZIP2_INCLUDE_DIRS})
+    set(BZIP2_LIBRARIES ${BZIP2_LIBRARIES})
 endif()
 
 if(USE_SYSTEM_Curses)
